0fcda0bc1506c973c7ef584b130c51a2157ab10c
Andy Strohman <astrohma@isovalent.com>
2026-05-05 20:18:49 +0000

    loader: link uprobe target by file descriptor
    In a later commit, we will check that the hash of the target matches the
    user specified digest via the file descriptor. This will ensure that we
    link the same target as what was we checked the digest against. This
    prevents a TOCTOU problem where the executable is replace after the
    digest check, but before linking.
    
    Signed-off-by: Andy Strohman <astrohma@isovalent.com>

--- pkg/sensors/program/loader_linux.go
+++ pkg/sensors/program/loader_linux.go
@@ -316,7 +316,21 @@ func uprobeAttachSingle(load *Program, prog *ebpf.Program, spec *ebpf.ProgramSpe
 	}
 
 	linkFn := func() (link.Link, error) {
-		exec, err := link.OpenExecutable(data.Path)
+		// The kernel tracks uprobe targets by inode. When we open a file,
+		// its file descriptor points to an inode.
+		// The loader API requires that a path is provided. It will not accept
+		// a file descriptor directly. But we can use the /proc/self/fd/<N> symlink
+		// to reference the file descriptor, which is a stable reference to the
+		// inode even if the path's inode changes.
+		// This trick ensures that the uprobe attachment is not affected by TOCTOU issues
+		// with the target file.
+		f, err := os.Open(data.Path)
+		if err != nil {
+			return nil, fmt.Errorf("open executable %s: %w", data.Path, err)
+		}
+		defer f.Close()
+		fdPath := fmt.Sprintf("/proc/self/fd/%d", f.Fd())
+		exec, err := link.OpenExecutable(fdPath)
 		if err != nil {
 			return nil, err
 		}
@@ -358,6 +372,49 @@ func MultiUprobeAttach(load *Program, bpfDir string) AttachFunc {
 	}
 }
 
+func attachSingleMultiUprobe(load *Program, prog *ebpf.Program, path string, attach *MultiUprobeAttachSymbolsCookies, bpfDir string, idx int, extra ...string) (link.Link, error) {
+	// The kernel tracks uprobe targets by inode. When we open a file,
+	// its file descriptor points to an inode.
+	// The loader API requires that a path is provided. It will not accept
+	// a file descriptor directly. But we can use the /proc/self/fd/<N> symlink
+	// to reference the file descriptor, which is a stable reference to the
+	// inode even if the path's inode changes.
+	// This trick ensures that the uprobe attachment is not affected by TOCTOU issues
+	// with the target file.
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("open executable %s: %w", path, err)
+	}
+	defer f.Close()
+	fdPath := fmt.Sprintf("/proc/self/fd/%d", f.Fd())
+	exec, err := link.OpenExecutable(fdPath)
+	if err != nil {
+		return nil, err
+	}
+	opts := &link.UprobeMultiOptions{
+		Addresses:     attach.Addresses,
+		Offsets:       attach.Offsets,
+		RefCtrOffsets: attach.RefCtrOffsets,
+		Cookies:       attach.Cookies,
+	}
+	var lnk link.Link
+	if load.RetProbe {
+		lnk, err = exec.UretprobeMulti(attach.Symbols, prog, opts)
+	} else {
+		lnk, err = exec.UprobeMulti(attach.Symbols, prog, opts)
+	}
+	if err != nil {
+		return nil, err
+	}
+	pinExtra := append(extra, strconv.Itoa(idx))
+	err = LinkPin(lnk, bpfDir, load, pinExtra...)
+	if err != nil {
+		lnk.Close()
+		return nil, err
+	}
+	return lnk, nil
+}
+
 func uprobeAttachMulti(load *Program, prog *ebpf.Program, spec *ebpf.ProgramSpec,
 	bpfDir string, extra ...string) (unloader.Unloader, error) {
 
@@ -368,32 +425,11 @@ func uprobeAttachMulti(load *Program, prog *ebpf.Program, spec *ebpf.ProgramSpec
 
 	linkFn := func() ([]link.Link, error) {
 		var links []link.Link
-		var lnk link.Link
 
 		idx := 0
 		for path, attach := range data.Attach {
-			exec, err := link.OpenExecutable(path)
+			lnk, err := attachSingleMultiUprobe(load, prog, path, attach, bpfDir, idx, extra...)
 			if err != nil {
-				return nil, err
-			}
-			opts := &link.UprobeMultiOptions{
-				Addresses:     attach.Addresses,
-				Offsets:       attach.Offsets,
-				RefCtrOffsets: attach.RefCtrOffsets,
-				Cookies:       attach.Cookies,
-			}
-			if load.RetProbe {
-				lnk, err = exec.UretprobeMulti(attach.Symbols, prog, opts)
-			} else {
-				lnk, err = exec.UprobeMulti(attach.Symbols, prog, opts)
-			}
-			if err != nil {
-				return nil, err
-			}
-			pinExtra := append(extra, strconv.Itoa(idx))
-			err = LinkPin(lnk, bpfDir, load, pinExtra...)
-			if err != nil {
-				lnk.Close()
 				return nil, err
 			}
 			links = append(links, lnk)
